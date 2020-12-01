7c9467f5642e1768a2796f618e2adc65c56c2ada
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2020-11-30 18:28:16 -0600

    initial files and added server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// Routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+// Test comment
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
